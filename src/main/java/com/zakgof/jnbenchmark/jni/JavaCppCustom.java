8982ab08af7f8695292c58ed502c64402ddc0964
Alexander Zakusylo <zakgof@gmail.com>
2019-05-13 15:26:47 +0300

    Optimize JavaCpp struct allocation

--- src/main/java/com/zakgof/jnbenchmark/jni/JavaCppCustom.java
+++ src/main/java/com/zakgof/jnbenchmark/jni/JavaCppCustom.java
@@ -1,10 +1,13 @@
 package com.zakgof.jnbenchmark.jni;
 
+import org.bytedeco.javacpp.Pointer;
+
 import com.zakgof.jnbenchmark.jni.gen.Kernel32Library;
 
 public class JavaCppCustom {
 
-	Kernel32Library.SYSTEMTIME preallocatedSystemTime;
+	private static final long SYSTEMTIME_STRUCT_LENGTH = 16; // precalculated as new SYSTEMTIME().sizeof();
+	private final Kernel32Library.SYSTEMTIME preallocatedSystemTime;
 
 	public JavaCppCustom() {
 		preallocatedSystemTime = new Kernel32Library.SYSTEMTIME();
@@ -15,7 +18,7 @@ public class JavaCppCustom {
 	}
 
 	public static short all() {
-		Kernel32Library.SYSTEMTIME systemtime = new Kernel32Library.SYSTEMTIME();
+		Kernel32Library.SYSTEMTIME systemtime = new Kernel32Library.SYSTEMTIME(Pointer.malloc(SYSTEMTIME_STRUCT_LENGTH));
 		Kernel32Library.GetSystemTime(systemtime);
 		return systemtime.wSecond();
 	}
--- src/main/java/com/zakgof/jnbenchmark/jni/JavaCppStock.java
+++ src/main/java/com/zakgof/jnbenchmark/jni/JavaCppStock.java
@@ -1,10 +1,12 @@
 package com.zakgof.jnbenchmark.jni;
 
+import org.bytedeco.javacpp.Pointer;
 import org.bytedeco.javacpp.windows;
 import org.bytedeco.javacpp.windows.SYSTEMTIME;
 
 public class JavaCppStock {
 
+	private static final long SYSTEMTIME_STRUCT_LENGTH = 16; // precalculated as new SYSTEMTIME().sizeof();
 	private SYSTEMTIME preallocatedSystemTime;
 
 	public JavaCppStock() {
@@ -16,7 +18,7 @@ public class JavaCppStock {
 	}
 
 	public static short all() {
-		SYSTEMTIME systemtime = new SYSTEMTIME();
+		SYSTEMTIME systemtime = new SYSTEMTIME(Pointer.malloc(SYSTEMTIME_STRUCT_LENGTH)); // new SYSTEMTIME() is much slower, see https://github.com/bytedeco/javacpp/issues/299
 		windows.GetSystemTime(systemtime);
 		return systemtime.wSecond();
 	}
